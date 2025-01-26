03e9d74fc46257852fa21fb64f78bf85d91d6029
Parth Maru <theparthmaru@gmail.com>
2025-01-26 16:36:04 +0000

    Handled response and status code using @ResponseStatus and ResponseEntity

--- src/main/java/com/example/TodoApiSpring/controller/TodoController.java
+++ src/main/java/com/example/TodoApiSpring/controller/TodoController.java
@@ -1,10 +1,9 @@
 package com.example.TodoApiSpring.controller;
 
 import com.example.TodoApiSpring.Todo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +25,6 @@ public class TodoController {
 
     /**
      * The ArrayList is being serialized and gets automatically converted to JSON response.
-     * @return
      */
 
     @GetMapping("/todos")
@@ -39,9 +37,21 @@ public class TodoController {
      * Incoming request body is of type Todo.
      * @RequestBody will do seriealize it in Todo form
      */
+
+    // Ways to provide response status
+    // 1. @ResponseStatus(HttpStatus.CREATED) below @PostMapping
+    // 2. Using ResponseEntity class manually sending response to the client
     @PostMapping("/todos")
+    @ResponseStatus(HttpStatus.CREATED)
     public Todo createTodo(@RequestBody Todo newTodo) {
          todoList.add(newTodo);
          return newTodo;
     }
+
+    // 2. Using ResponseEntity class manually sending response to the client
+    // Handles things to return to the client manually
+//    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo) {
+//        todoList.add(newTodo);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+//    }
 }
